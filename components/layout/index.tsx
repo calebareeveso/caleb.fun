0736cd5a58206a1a3be597471c1406653b818f90
Caleb Areeveso <58891908+calebareeveso@users.noreply.github.com>
2023-02-24 13:09:57 +0000

    feat: update README

---
+++ components/layout/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/legacy/image";
+import Link from "next/link";
+import React from "react";
+
+
+export default function Main({ children, ...props }:any) {
+    return (
+        <main className="wrapper">
+        <div className="max-w-1xl mx-auto container px-4">
+          <div className="container mx-auto">
+       {children}
+       </div>
+      </div>
+       </main>
+  );
+}
